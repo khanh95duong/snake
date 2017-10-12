37c561b321caff8db267496dd0f823c84ff92782
Khanh Duong <kduong@mymail.mines.edu>
2017-10-12 17:38:55 -0600

    Implemented growing function

--- snake.js
+++ snake.js
@@ -5,6 +5,8 @@ var snakeSize = 25;
 
 // State of game, true until game is over
 var game = true;
+// Flag if a food has been eaten
+var fed = false;
 
 // Snake Location
 var x = 0;
@@ -17,6 +19,37 @@ var sheight = canvas.height;
 // Direction variable
 // up = 1, right = 2, down = 3, left = 4
 var direction;
+
+var bodies = [];
+
+function constructor(body, xx, yy) {
+    body.px = xx;
+    body.py = yy;
+}
+
+function newparent(body, parent) {
+    body.px = parent.bx;
+    body.py = parent.by;
+}
+
+function np(body) {
+    body.px = x;
+    body.py = y;
+}
+
+function shift(body) {
+    body.bx = body.px;
+    body.by = body.py;
+}
+
+function drawBody(body) {
+    ctx.beginPath();
+    ctx.fillStyle = "#00FF00";
+    ctx.rect(body.bx, body.by, snakeSize, snakeSize);
+    ctx.fill();
+    ctx.stroke();
+}
+
 
 function move(e) {
     switch (e.keyCode) {
@@ -36,17 +69,10 @@ function move(e) {
             // down key pressed
             direction = 3;
             break;
-        case 32:
-            update();
+        case 81:
+            fed = true;
             break;
     }
-}
-
-function drawBody() {
-    ctx.beginPath();
-    ctx.fillStyle = "#00FF00";
-    ctx.rect(x, y, snakeSize, snakeSize);
-    ctx.fill();
 }
 
 function drawHead() {
@@ -54,16 +80,38 @@ function drawHead() {
     ctx.fillStyle = "#FF0000";
     ctx.rect(x, y, snakeSize, snakeSize);
     ctx.fill();
+    ctx.stroke();
 }
 
 function draw() {
     ctx.clearRect(0, 0, 600, 600);
-
     // Drawing Snake
     drawHead();
+    if (bodies[0] != null) {
+        shift(bodies[0]);
+        np(bodies[0]);
+        drawBody(bodies[0]);
+        for (i = 1; i < bodies.length; i++) {
+            shift(bodies[i])
+            newparent(bodies[i], bodies[i - 1]);
+            drawBody(bodies[i]);
+        }
+    }
 }
 
 function update() {
+    if (fed) {
+        fed = false;
+        for (i = 0; i < 5; i++) {
+            var newBody = {
+                bx: -100,
+                by: -100,
+                px: -100,
+                py: -100
+            }
+            bodies[bodies.length] = newBody;
+        }
+    }
     switch (direction) {
         case 1:
             y -= 25;
@@ -85,5 +133,12 @@ function init() {
     if (direction == null) {
         direction = 3;
         setInterval("update()", 100);
+        var newBody = {
+            bx: -100,
+            by: -100,
+            px: -100,
+            py: -100
+        }
+        bodies[0] = newBody;
     }
 }
