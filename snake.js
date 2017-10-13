316bee3415dd45b21ed18b5dc058c7f7a6f7fb68
Khanh Duong <kduong@mymail.mines.edu>
2017-10-13 14:10:55 -0600

    Killed speedrun community

--- snake.js
+++ snake.js
@@ -10,6 +10,8 @@ var game = true;
 var fed = false;
 // Is the game over
 var over = false;
+// Buffer so you can't move too quickly
+var alreadyMove = false;
 
 // Snake Location
 var x = 0;
@@ -60,19 +62,31 @@ function move(e) {
     switch (e.keyCode) {
         case 37:
             // left key pressed
-            if (direction != 2) direction = 4;
+            if (direction != 2 && !alreadyMove) {
+				direction = 4;
+				alreadyMove = true;
+			}
             break;
         case 38:
             // up key pressed
-            if (direction != 3) direction = 1;
+            if (direction != 3 && !alreadyMove) {
+				direction = 1;
+				alreadyMove = true;
+			}
             break;
         case 39:
             // right key pressed
-            if (direction != 4) direction = 2;
+            if (direction != 4 && !alreadyMove) {
+				direction = 2;
+				alreadyMove = true;
+			}
             break;
         case 40:
             // down key pressed
-            if (direction != 1) direction = 3;
+            if (direction != 1 && !alreadyMove) {
+				direction = 3;
+				alreadyMove = true;
+			}
             break;
         case 81:
             fed = true;
@@ -89,9 +103,6 @@ function drawHead() {
 }
 
 function drawFoods() {
-	var remainderX = foodX % 25;
-	var remainderY = foodY % 25;
-	var j = 0;
 	
 	if (foodX < 0) {
 		foodX *= -1;
@@ -99,7 +110,7 @@ function drawFoods() {
 	if (foodY < 0) {
 		foodY *= -1;
 	}
-	
+		
 	if (foodX + 25 > swidth) {
 		foodX = swidth - 25;
 	}
@@ -107,12 +118,9 @@ function drawFoods() {
 		foodY = sheight - 25;
 	}
 	
-	//while (foodX + 50 >= swidth || foodX <= 0) {
-	//	foodX = Math.floor(Math.random() * swidth);
-	//}
-	//while (foodY + 50 >= swidth || foodY <= 0) {
-	//	foodY = Math.floor(Math.random() * swidth);
-	//}
+	var remainderX = foodX % 25;
+	var remainderY = foodY % 25;
+	var j = 0;
 	
 	if (remainderX <= 12) {
 		foodX -= remainderX;
@@ -128,6 +136,13 @@ function drawFoods() {
 		remainder = 25 - remainderY;
 		foodY += remainderY;
 	}
+	
+	//while (foodX + 50 >= swidth || foodX <= 0) {
+	//	foodX = Math.floor(Math.random() * swidth);
+	//}
+	//while (foodY + 50 >= swidth || foodY <= 0) {
+	//	foodY = Math.floor(Math.random() * swidth);
+	//}
 	ctx.beginPath();
     ctx.fillStyle = "#FF8C00";
     ctx.rect(foodX, foodY, foodSize, foodSize);
@@ -167,7 +182,7 @@ function fade() {
 }
 
 function update() {
-	
+	alreadyMove = false;
 	if (bodies[0].px === foodX && bodies[0].py === foodY) {
 		foodX = Math.floor(Math.random() * swidth);
 		foodY = Math.floor(Math.random() * sheight);
